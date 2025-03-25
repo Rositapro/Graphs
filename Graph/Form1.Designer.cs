2d581a3636357cafde87f387d31d13975a4c2e41
Rosalinda <i23050333@monclova.tecnm.mx>
2025-03-24 21:30:29 -0600

    Agregar archivos de proyecto.

---
+++ Graph/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            fmpEngine = new ScottPlot.WinForms.FormsPlot();
+            fmpAspiration = new ScottPlot.WinForms.FormsPlot();
+            fmpNumofDoors = new ScottPlot.WinForms.FormsPlot();
+            fmpNormalizedLosses = new ScottPlot.WinForms.FormsPlot();
+            fmp1FuelType = new ScottPlot.WinForms.FormsPlot();
+            dtgvAutomovile = new DataGridView();
+            btnFilter = new Button();
+            cmbFilter = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dtgvAutomovile).BeginInit();
+            SuspendLayout();
+            // 
+            // fmpEngine
+            // 
+            fmpEngine.DisplayScale = 1F;
+            fmpEngine.Location = new Point(908, 330);
+            fmpEngine.Name = "fmpEngine";
+            fmpEngine.Size = new Size(218, 204);
+            fmpEngine.TabIndex = 0;
+            // 
+            // fmpAspiration
+            // 
+            fmpAspiration.DisplayScale = 1F;
+            fmpAspiration.Location = new Point(460, 330);
+            fmpAspiration.Name = "fmpAspiration";
+            fmpAspiration.Size = new Size(218, 204);
+            fmpAspiration.TabIndex = 1;
+            // 
+            // fmpNumofDoors
+            // 
+            fmpNumofDoors.DisplayScale = 1F;
+            fmpNumofDoors.Location = new Point(684, 330);
+            fmpNumofDoors.Name = "fmpNumofDoors";
+            fmpNumofDoors.Size = new Size(218, 204);
+            fmpNumofDoors.TabIndex = 2;
+            // 
+            // fmpNormalizedLosses
+            // 
+            fmpNormalizedLosses.DisplayScale = 1F;
+            fmpNormalizedLosses.Location = new Point(236, 330);
+            fmpNormalizedLosses.Name = "fmpNormalizedLosses";
+            fmpNormalizedLosses.Size = new Size(218, 204);
+            fmpNormalizedLosses.TabIndex = 3;
+            // 
+            // fmp1FuelType
+            // 
+            fmp1FuelType.DisplayScale = 1F;
+            fmp1FuelType.Location = new Point(12, 330);
+            fmp1FuelType.Name = "fmp1FuelType";
+            fmp1FuelType.Size = new Size(218, 204);
+            fmp1FuelType.TabIndex = 4;
+            // 
+            // dtgvAutomovile
+            // 
+            dtgvAutomovile.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgvAutomovile.Location = new Point(236, 32);
+            dtgvAutomovile.Name = "dtgvAutomovile";
+            dtgvAutomovile.Size = new Size(654, 262);
+            dtgvAutomovile.TabIndex = 5;
+            // 
+            // btnFilter
+            // 
+            btnFilter.Location = new Point(155, 32);
+            btnFilter.Name = "btnFilter";
+            btnFilter.Size = new Size(75, 23);
+            btnFilter.TabIndex = 7;
+            btnFilter.Text = "Filter";
+            btnFilter.UseVisualStyleBackColor = true;
+            btnFilter.Click += btnFilter_Click_1;
+            // 
+            // cmbFilter
+            // 
+            cmbFilter.FormattingEnabled = true;
+            cmbFilter.Items.AddRange(new object[] { "Toyota", "Nissan", "Mazda", "Mitsubishi", "Honda", "Volkswagen", "Subaru", "Peugot", "Volvo", "Dodge", "Mercedes-Benz", "BMW", "Audi", "Plymouth", "Saab", "Porsche", "Isuzu", "Jaguar", "Chevrolet", "Alfa Romeo", "Renault", "Mercury" });
+            cmbFilter.Location = new Point(25, 32);
+            cmbFilter.Name = "cmbFilter";
+            cmbFilter.Size = new Size(121, 23);
+            cmbFilter.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(91, 312);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Fuel type";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(91, 312);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Fuel type";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(312, 312);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 15);
+            label1.TabIndex = 11;
+            label1.Text = "Normalized losses";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(546, 312);
+            label4.Name = "label4";
+            label4.Size = new Size(61, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Aspiration";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(990, 312);
+            label5.Name = "label5";
+            label5.Size = new Size(105, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Engine localitation";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(758, 312);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 15);
+            label6.TabIndex = 14;
+            label6.Text = "num of doors";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1231, 537);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(cmbFilter);
+            Controls.Add(btnFilter);
+            Controls.Add(dtgvAutomovile);
+            Controls.Add(fmp1FuelType);
+            Controls.Add(fmpNormalizedLosses);
+            Controls.Add(fmpNumofDoors);
+            Controls.Add(fmpAspiration);
+            Controls.Add(fmpEngine);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dtgvAutomovile).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot fmpEngine;
+        private ScottPlot.WinForms.FormsPlot fmpAspiration;
+        private ScottPlot.WinForms.FormsPlot fmpNumofDoors;
+        private ScottPlot.WinForms.FormsPlot fmpNormalizedLosses;
+        private ScottPlot.WinForms.FormsPlot fmp1FuelType;
+        private DataGridView dtgvAutomovile;
+        private Button btnFilter;
+        private ComboBox cmbFilter;
+        private Label label2;
+        private Label label3;
+        private Label label1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Graph/Form1.cs
@@ -0,0 +1,99 @@
+using ScottPlot;
+using ScottPlot.WinForms;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillTable();
+        }
+
+        private void FillTable()
+        {
+            string path = "C:\\Users\\rosal\\Downloads\\Automobile_data.CSV";
+            string[] lines = File.ReadAllLines(path);
+            string[] headers = lines[0].Split(',');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] data = lines[i].Split(',');
+                DataRow row = dt.NewRow();
+                for (int j = 0; j < headers.Length; j++)
+                    row[j] = data[j];
+                dt.Rows.Add(row);
+            }
+            dtgvAutomovile.DataSource = dt;
+        }
+       
+        private void btnFilter_Click_1(object sender, EventArgs e)
+        {
+            string selectedBrand = cmbFilter.Text;
+            DataTable dt = (DataTable)dtgvAutomovile.DataSource;
+
+            if (dt != null)
+            {
+                DataView dv = dt.DefaultView;
+                dv.RowFilter = $"Make = '{selectedBrand}'";
+                DataTable filterTable = dv.ToTable();
+                ProcessData(filterTable, "fuel-type", fmp1FuelType);
+                ProcessData(filterTable, "aspiration", fmpNormalizedLosses);
+                ProcessData(filterTable, "normalized-losses", fmpAspiration);
+                ProcessData(filterTable, "num-of-doors", fmpNumofDoors);
+                ProcessData(filterTable, "engine-location", fmpEngine);
+            }
+
+        }
+        private void ProcessData(DataTable table, string columnName, FormsPlot plot)
+        {
+            Dictionary<string, double> countData = new Dictionary<string, double>();
+            foreach (DataRow row in table.Rows)
+            {
+                string value = row[columnName].ToString().Trim().ToLower();
+
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (countData.ContainsKey(value))
+                        countData[value]++;
+                    else
+                        countData[value] = 1;
+                }
+            }
+            double[] values = countData.Values.ToArray();
+            string[] labels = countData.Keys.ToArray();
+            CreatePie(values, labels, plot, columnName);
+        }
+        private void CreatePie(double[] values, string[] labels, FormsPlot plot, string title)
+        {
+            plot.Plot.Clear();
+            var pie = plot.Plot.Add.Pie(values);
+            pie.ExplodeFraction = .1;
+            pie.SliceLabelDistance = 0.5;
+
+            double total = values.Sum();
+            double[] percentages = values.Select(x => x / total * 100).ToArray();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                pie.Slices[i].Label = $"{labels[i]} ({percentages[i]:0.0}%)";
+                pie.Slices[i].LabelFontSize = 14;
+                pie.Slices[i].LabelBold = true;
+                pie.Slices[i].LabelFontColor = Colors.Green.WithAlpha(.7);
+            }
+            plot.Plot.Title("Distribution of " + title);
+            plot.Plot.Axes.Frameless();
+            plot.Plot.HideGrid();
+            plot.Plot.Axes.AutoScale();
+            plot.Refresh();
+        }
+    }
+}
